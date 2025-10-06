96b4a430b813978cee578262018fbd89c89006cc
weston273 <sululuweston@gmail.com>
2025-10-06 11:27:40 +0200

    almost finished home page

--- agrimo-site/src/components/ExtraInfo.jsx
+++ agrimo-site/src/components/ExtraInfo.jsx
@@ -2,33 +2,48 @@ import React from 'react'
 import './ExtraInfo.css'
 import WhoWeAreSection from './above-title/WhoWeAreSection'
 import LeafIcon from './../assets/images/leaf-icon.png'
+import FarmerImage from './../assets/images/farmers-bg.jpg' // replace with your farmer image
 
 const ExtraInfo = () => {
   return (
     <>
-      <section className='ExtraInfoContainer'>
-        <div className='extra-info-above-container'>
-          {/* Above Title */}
-        <div className='extra-info-above-title'>
-          <WhoWeAreSection icon={LeafIcon} title='What We Do' />
+      <section className='ExtraInfoSection'>
+        {/* Left Image Section */}
+        <div className='extra-info-left'>
+          <img src={FarmerImage} alt='Farmer working on field' />
         </div>
 
-        <div className='extra-info-header-text-container'>
-          <p className='extra-info-header-text'>
-            Healthy life with fresh <br />
-            products.
-          </p>
-        </div>
+        {/* Right Yellow Info Box */}
+        <div className='extra-info-right'>
+          <div className='extra-info-content'>
+            <WhoWeAreSection icon={LeafIcon} title='What We Do' />
 
-        <div className='extra-info-paragraph-text'>
-          <p>
-            There are many variations of passages of Lorem Ipsum available , but the <br />
-            majority have suffered alteration in some form, by injected humour, or < br />
-            randomised words which we don't look even.
-          </p>
+            <h2 className='extra-info-header-text'>
+              Healthy life with fresh <br /> products
+            </h2>
+
+            <p className='extra-info-paragraph'>
+              There are many variations of passages of Lorem Ipsum available, but the
+              majority have suffered alteration in some form, by injected humour, or
+              randomised words which don’t look even.
+            </p>
+
+            <div className='extra-info-stats'>
+              <div className='info-stat'>
+                <div className='stat-circle'>90%</div>
+                <p>
+                  Eco Farms <br /> Worldwide
+                </p>
+              </div>
+              <div className='info-stat'>
+                <div className='stat-circle'>78%</div>
+                <p>
+                  Special <br /> Equipment
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-        
       </section>
     </>
   )
